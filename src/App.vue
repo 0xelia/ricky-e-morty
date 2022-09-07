2db68d2f4d06d73e25da25473e73708b1c385260
0xelia <elia.vanon@yahoo.com>
2022-09-07 10:35:33 +0200

    fonts added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-
+    <HelloWorld :msg="title"></HelloWorld>
   </div>
 </template>
 
 <script>
-
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-  
+    HelloWorld
+  },
+
+  data(){
+    return{
+      title:'Ricky & Morty'
+    }
   }
 }
 </script>
 
 <style lang="scss">
+  @import './style/generals.scss';
 
 </style>
